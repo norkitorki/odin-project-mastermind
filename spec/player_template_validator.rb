dc353ea5972b46074c96fb3cbaf14283e9b6f9ed
norkitorki <norkitorki@tutanota.de>
2021-05-13 08:48:03 +0200

    Delete unnecessary tests

--- spec/player_template_validator.rb
+++
@@ -1,9 +0,0 @@
-module PlayerTemplateValidator
-  def name_defined?(object)
-    object.respond_to?(:name)
-  end
-
-  def piece_defined?(object)
-    object.respond_to?(:piece)
-  end
-end
--- spec/scoreboard_template_validator.rb
+++
@@ -1,9 +0,0 @@
-module ScoreboardTemplateValidator
-  def to_s_defined?(object)
-    object.respond_to?(:to_s)
-  end
-
-  def update_score_defined?(object)
-    object.respond_to?(:update_score)
-  end
-end
